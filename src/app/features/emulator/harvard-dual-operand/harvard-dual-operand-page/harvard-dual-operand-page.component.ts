58d91048e1597d8e69dfe3ab0c5e19213060aac6
s.berkunov <studiorain27@gmail.com>
2024-12-04 18:11:42 +0300

    feat(harvard-dual): add emulator

--- src/app/features/emulator/harvard-dual-operand/harvard-dual-operand-page/harvard-dual-operand-page.component.ts
+++ src/app/features/emulator/harvard-dual-operand/harvard-dual-operand-page/harvard-dual-operand-page.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {HarvardDualOperandService} from '../harvard-dual-operand.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-harvard-dual-operand-page',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './harvard-dual-operand-page.component.html',
   styleUrl: './harvard-dual-operand-page.component.scss'
 })
 export class HarvardDualOperandPageComponent {
+  asm = inject(HarvardDualOperandService);
 
+  loadData(inputData: string) {
+    const data: number[] = inputData.split(',').map(el => {
+      return parseInt(el);
+    });
+
+    this.asm.loadData(data);
+  }
+
+  loadInstructions(inputInstructions: string) {
+    const instructions: string[] = inputInstructions.split(/\r?\n|\r|\n/g);
+
+    this.asm.loadInstructions(instructions);
+  }
+
+  launch() {
+    this.asm.execute();
+  }
+
+  launchInterval() {
+
+  }
+
+  launchOneInstruction() {
+
+  }
+
+  resetAll() {
+    this.asm.setMemorySize(0);
+  }
 }
--- src/app/features/emulator/harvard-dual-operand/harvard-dual-operand.service.ts
+++ src/app/features/emulator/harvard-dual-operand/harvard-dual-operand.service.ts
@@ -4,6 +4,65 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HarvardDualOperandService {
+  private memoryData: number[] = [];
+  private memoryInstructions: string[] = [];
+  private registers: { [key: string]: number } = { ACC: 0, R1: 0, R2: 0, TMP: 0 };
 
-  constructor() { }
+  setMemorySize(memorySize: number) {
+    this.memoryData = new Array(memorySize).fill(0);
+  }
+
+  get memorySize() {
+    return this.memoryData.length;
+  }
+
+  getInstructions() {
+    return this.memoryInstructions;
+  }
+
+  getRegisters() {
+    return this.registers;
+  }
+
+  loadData(data: number[]) {
+    this.memoryData = [...data];
+  }
+
+
+  loadInstructions(instructions: string[]) {
+    this.memoryInstructions = [...instructions];
+  }
+
+  execute() {
+    for (const instruction of this.memoryInstructions) {
+      const [op, arg1, arg2] = instruction.split(" ");
+      switch (op) {
+        case "LOAD":
+          this.registers[arg1] = this.memoryData[parseInt(arg2)];
+          break;
+        case "ADD":
+          this.registers[arg1] += this.registers[arg2];
+          break;
+        case "MUL":
+          this.registers[arg1] *= this.registers[arg2];
+          break;
+        case "STORE":
+          this.memoryData[parseInt(arg2)] = this.registers[arg1];
+          break;
+        case "RESET":
+          this.registers[arg1] = 0;
+          break;
+        default:
+          throw new Error(`Неизвестная инструкция: ${op}`);
+      }
+    }
+  }
+
+  getMemory(): number[] {
+    return [...this.memoryData];
+  }
+
+  getRegisterValue(register: string): number {
+    return this.registers[register];
+  }
 }
